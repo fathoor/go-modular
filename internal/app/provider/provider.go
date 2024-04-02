d7342998dcc2a408168f61ddbd5d007a341fca2b
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-02 21:39:30 +0700

    chore(provider): container provider

---
+++ internal/app/provider/provider.go
@@ -0,0 +1,17 @@
+package provider
+
+import (
+	"github.com/fathoor/go-modular/internal/app/config"
+	"github.com/gofiber/fiber/v3"
+	"github.com/jmoiron/sqlx"
+)
+
+type Provider struct {
+	App       *fiber.App
+	Config    *config.Config
+	DB        *sqlx.DB
+	Validator *config.Validator
+}
+
+func (p *Provider) Provide() {
+}
