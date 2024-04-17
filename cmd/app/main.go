25da9eb05f27867e7198c6137cd4edd647f7c7a3
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-17 21:39:04 +0700

    chore(cmd): pass config to fiber

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	var (
 		cfg       = config.NewConfig()
-		fiber     = config.NewFiber()
+		fiber     = config.NewFiber(cfg)
 		postgres  = config.NewPostgres(cfg)
 		validator = config.NewValidator()
 		bootstrap = provider.Provider{App: fiber, Config: cfg, DB: postgres, Validator: validator}
@@ -23,7 +23,7 @@ func main() {
 			log.Fatalf("Failed to close database connection: %v", err)
 		}
 	}(postgres)
-	
+
 	bootstrap.Provide()
 
 	if err := fiber.Listen(fmt.Sprintf(":%d", cfg.GetInt("APP_PORT", 8080))); err != nil {
